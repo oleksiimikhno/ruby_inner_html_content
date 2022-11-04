1411e803ca725966a20c0b55cb6eeea57f07edb9
Oleksii Mikhno <xioflesh@gmail.com>
2022-11-04 14:18:52 +0200

    add erb template

--- inner_html_content.gemspec
+++ inner_html_content.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
     s.name        = "inner_html_content"
-    s.version     = "0.0.1"
+    s.version     = "0.1.0"
     s.summary     = "inner_html_content!"
     s.description = "Gem Inner_html content!"
     s.authors     = ["Oleksii Mikhno"]
--- lib/inner_html_content.rb
+++ lib/inner_html_content.rb
@@ -1,64 +1,26 @@
 # frozen_string_literal: true
 
 require 'erb'
-
-template = %{
-  <html>
-    <head><title>Ruby Toys -- <%= @name %></title></head>
-    <body>
-
-      <h1><%= @name %> (<%= @code %>)</h1>
-      <p><%= @desc %></p>
-
-      <ul>
-        <% @features.each do |f| %>
-          <li><b><%= f %></b></li>
-        <% end %>
-      </ul>
-
-      <p>
-        <% if @cost < 10 %>
-          <b>Only <%= @cost %>!!!</b>
-        <% else %>
-           Call for a price, today!
-        <% end %>
-      </p>
-
-    </body>
-  </html>
-}.gsub(/^  /, '')
-
 # Gem append content in html body
 module InnerHTMLContent
   class << self
-    def add_content(content, bypass_html: true, file_name: 'index.html')
-      serialize_content = content.gsub(%r{<\w+>|</\w+>}, '')
-      content = bypass_html && serialize_content ? content.gsub(%r{<\w+>|</\w+>}, '') : content
+    def add_content(title, content, bypass_html: true, file_name: 'index.html')
+      @title = title
+      @content = content
 
-      template_html(file_name, content)
+      serialize_content = @content.gsub(%r{<\w+>|</\w+>}, '')
+      @content = bypass_html && serialize_content ? @content.gsub(%r{<\w+>|</\w+>}, '') : @content
+
+      create_html(file_name)
     end
 
     private
 
-    def template_html(file_name, content)
-      template = '<!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>{title_page}</title>
-        </head>
-        <body>
-        {inner_content}
-        </body>
-      </html>'.gsub(/^      /, '')
-      template = template.gsub('{title_page}', content)
-      template = template.gsub('{inner_content}', content)
+    def create_html(file_name)
+      template = File.read('view/template.erb')
+      result = ERB.new(template).result(binding)
 
-      File.open(file_name, 'w') do |file|
-        file.write(template)
-      end
+      File.write(file_name, result)
     end
   end
 end
