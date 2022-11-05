a51c8d04dc49056251c7169f471f59578b245977
Oleksii Mikhno <xioflesh@gmail.com>
2022-11-05 14:53:30 +0200

    fix bug style

--- lib/inner_html_content.rb
+++ lib/inner_html_content.rb
@@ -25,7 +25,7 @@ module InnerHTMLContent
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title><%= @title %></title>
-        <link rel="stylesheet" href="<%= @style_path %> />
+        <link rel="stylesheet" href="<%= @style_path %>" />
       </head>
       <body>
       <%= @content %>
